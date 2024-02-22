32169f26e64b32fb06857fa7686c01dcb77236fc
Kenneth Atchon <kenneth.atchon@hotmail.com>
2024-02-22 16:00:58 -0600

    First commit, added some changes

--- Lab4_Group1.java
+++ Lab4_Group1.java
@@ -3,14 +3,17 @@
 public class Lab4_Group1
 {
     // CHRISTIAN
-    public static int addNumbers(int num1, num2){
-        return num1+num2
+    public static int addNumbers(int num1, int num2){
+        return num1+num2;
     }
 
 
     //   KENENTH
-    public static void printHello(){
-        System.out.println("Hello");
+    public static void printHelloWorld(int number){
+        for(int i = 0; i < 5; ++i){
+            System.out.println("Hello World");
+        }
+        
     }
   
     public static int subNumb(int a, int b){
